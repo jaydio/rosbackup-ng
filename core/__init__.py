c9f308b298ab70370ff346ec95841ac87cae6b06
JD Bungart <jd@pronetivity.ph>
2025-01-08 01:30:30 +0800

    refactor: update core package exports
    - Add BackupProgressHandler to exports
    - Organize imports by module
    - Add type definitions to exports

--- core/__init__.py
+++ core/__init__.py
@@ -4,21 +4,36 @@ Core utilities for RouterOS backup operations.
 
 __version__ = '0.1.0'
 
-from .backup_utils import BackupManager
-from .ssh_utils import SSHManager
-from .router_utils import RouterInfoManager
-from .notification_utils import NotificationManager
+from .backup_utils import (
+    BackupManager,
+    RouterInfoManager,
+    TargetConfig,
+    BackupConfig,
+    SMTPConfig,
+    NotificationConfig
+)
 from .logging_utils import LogManager
-from .shell_utils import ColoredFormatter, ShellPbarHandler
-from .compose_utils import ComposeStyleHandler
+from .shell_utils import (
+    ColoredFormatter,
+    ShellPbarHandler,
+    ComposeStyleHandler,
+    BackupProgressHandler
+)
+from .notification_utils import NotificationManager
+from .ssh_utils import SSHManager
 
 __all__ = [
     'BackupManager',
-    'SSHManager',
     'RouterInfoManager',
-    'NotificationManager',
     'LogManager',
+    'NotificationManager',
+    'SSHManager',
     'ColoredFormatter',
     'ShellPbarHandler',
-    'ComposeStyleHandler'
+    'ComposeStyleHandler',
+    'BackupProgressHandler',
+    'TargetConfig',
+    'BackupConfig',
+    'SMTPConfig',
+    'NotificationConfig'
 ]
